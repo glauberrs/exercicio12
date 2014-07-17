862925b2be941d9f8e8159c7db21b60fce5f2891
Jackson F. de A. Mafra <jacksonfdam@gmail.com>
2014-07-17 18:41:14 -0300

    Initial Commit

---
+++ exercicio12/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  exercicio12
+//
+//  Created by Jackson F. de A. Mafra on 17/07/2014.
+//  Copyright (c) 2014 Jackson F. de A. Mafra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ exercicio12/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  exercicio12
+//
+//  Created by Jackson F. de A. Mafra on 17/07/2014.
+//  Copyright (c) 2014 Jackson F. de A. Mafra. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
